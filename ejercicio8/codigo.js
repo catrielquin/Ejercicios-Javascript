508f1422e3c501496e74dad11595eb6e2be8d8c8
jonathan catriel quintana <111572681+catrielquin@users.noreply.github.com>
2022-10-03 07:44:22 -0300

    Add files via upload

---
+++ ejercicio8/codigo.js
@@ -0,0 +1,11 @@
+numero = prompt("ingrese un numero: ")
+parOImpar(numero)
+
+function parOImpar(n){
+
+    if ((n % 2) == 0 ){
+        alert("el numero es par")
+    }else{
+        alert("el numero es impar")
+    }
+}
